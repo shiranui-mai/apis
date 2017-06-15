f4d32938ae2a03b9b459a877ef1401eac8b8acfe
fish <fish@debian>
2017-06-15 11:58:49 +0800

    omit

--- outside/ngx_http_outside_module.c
+++ outside/ngx_http_outside_module.c
@@ -18,7 +18,6 @@ static ngx_int_t invoke_subrequest(ngx_http_request_t* r, outside_service_t* s)
 	psr->handler = outside_subrequest_post_handler;
     psr->data = s;
 
-    // r->count = 1;
     // Invoke subrequest
     ngx_http_request_t* tmp;
     return ngx_http_subrequest(r, &s->uri, NULL, &tmp, psr, NGX_HTTP_SUBREQUEST_IN_MEMORY);
@@ -32,36 +31,8 @@ static ngx_int_t ngx_http_outside_handler(ngx_http_request_t* r)
         return NGX_ERROR;
     }
 
-    // r->write_event_handler = outside_post_handler;
-    // r->write_event_handler = ngx_http_request_empty_handler;
-    // r->write_event_handler = ngx_http_request_finalizer;
-
     //  Invoke first subrequest
     return invoke_subrequest(r, (outside_service_t*)cf);
-	// return (invoke_subrequest(r, (outside_service_t*)cf) != NGX_OK)?NGX_ERROR:NGX_DONE;
-	// return (invoke_subrequest(r, (outside_service_t*)&(cf->finish)) != NGX_OK)?NGX_ERROR:NGX_DONE;
-}
-
-static ngx_int_t ngx_http_outside_test_handler(ngx_http_request_t* r) 
-{
-	r->headers_out.content_type.len = sizeof("text/plain") - 1;
-	r->headers_out.content_type.data = (u_char*)"text/plain";
-
-	r->headers_out.status = NGX_HTTP_OK;
-	r->connection->buffered |= NGX_HTTP_WRITE_BUFFERED;
-
-    ngx_buf_t* b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
-    b->pos = (u_char*)"out_side_test";
-    b->last = b->pos + sizeof("out_side_test") - 1;
-    b->memory = 1;
-    b->last_buf = 1;
-	
-	ngx_chain_t out; 
-    out.buf = b;
-    out.next = NULL;
-
-	ngx_http_send_header(r);
-    return ngx_http_output_filter(r, &out);
 }
 
 static void* ngx_http_outside_create_loc_conf(ngx_conf_t* cf)
@@ -92,18 +63,10 @@ static char* ngx_http_outside(ngx_conf_t* cf, ngx_command_t* cmd, void* conf)
 	ngx_conf_set_str_slot(cf, cmd, conf);
 	return NGX_CONF_OK;
 }
-static char* ngx_http_outside_test(ngx_conf_t* cf, ngx_command_t* cmd, void* conf)
-{
-	ngx_http_core_loc_conf_t* clct = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module); 
-	clct->handler = ngx_http_outside_test_handler;
-	// ngx_conf_set_str_slot(cf, cmd, conf);
-	return NGX_CONF_OK;
-}
 
 // post handler
 static void outside_post_handler(ngx_http_request_t* r)
 {
-    // r->write_event_handler = ngx_http_request_empty_handler;
 	if (r->headers_out.status != NGX_HTTP_OK)
 	{
 		ngx_http_finalize_request(r, r->headers_out.status);
@@ -121,20 +84,12 @@ static void outside_post_handler(ngx_http_request_t* r)
 
     // Reset last_buf flag is 1
     if (out_l != NULL) {
-        ngx_log_error(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "[tzj] [outside_post_handler] out_l != NULL. \n");
         get_last_chain(out_l)->buf->last_buf = 1;
-    } else {
-        ngx_log_error(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "[tzj] [outside_post_handler] out_l == NULL. \n");
     }
 
-    // r->out = NULL;
 	ngx_http_send_header(r);
     ngx_int_t ret = ngx_http_output_filter(r, out_l);
-    // ngx_http_output_filter(r, out_l);
-	// ngx_http_finalize_request(r, ngx_http_output_filter(r, out_l));
-	// ngx_http_finalize_request(r, ngx_http_output_filter(r, NULL));
 	ngx_http_finalize_request(r, ret);
-    // ngx_http_close_request(r, NGX_HTTP_OK);
 }
 
 // Child post handler
@@ -144,12 +99,9 @@ static ngx_int_t outside_subrequest_post_handler(ngx_http_request_t* r, void* da
 	ngx_http_request_t* pr = r->parent;
 
     pr->headers_out.status = r->headers_out.status;
-    // pr->write_event_handler = s->fin == 1?outside_post_handler:ngx_http_request_empty_handler;
 
-    // pr->out = NULL;
     ngx_chain_add_copy(pr->pool, (ngx_chain_t**)&pr->user_data, r->upstream->out_bufs);
 
-        // outside_post_handler(pr);
     if (s->fin == 1) {
         outside_post_handler(pr);
     } else {
@@ -157,7 +109,6 @@ static ngx_int_t outside_subrequest_post_handler(ngx_http_request_t* r, void* da
         return invoke_subrequest(pr, ++s);
     }
 
-    // pr->write_event_handler = outside_post_handler;
     return NGX_OK;
 }
 
--- outside/outside.h
+++ outside/outside.h
@@ -24,7 +24,6 @@ static void* ngx_http_outside_create_loc_conf(ngx_conf_t* cf);
 static char* ngx_http_outside_merge_loc_conf(ngx_conf_t* cf, void* parent, void* child);
 
 static char* ngx_http_outside(ngx_conf_t* cf, ngx_command_t* cmd, void* conf);
-static char* ngx_http_outside_test(ngx_conf_t* cf, ngx_command_t* cmd, void* conf);
 
 // post handler
 static void outside_post_handler(ngx_http_request_t* r);
@@ -46,14 +45,6 @@ static ngx_command_t ngx_http_outside_commands[] = {
 		ngx_http_outside,
 		NGX_HTTP_LOC_CONF_OFFSET,
 		offsetof(ngx_http_outside_conf_t, finish.uri),
-		NULL
-	},
-	{
-		ngx_string("outside_test"),
-		NGX_HTTP_LOC_CONF | NGX_CONF_NOARGS,
-		ngx_http_outside_test,
-		0,
-		0,
 		NULL
 	},
 	ngx_null_command	
